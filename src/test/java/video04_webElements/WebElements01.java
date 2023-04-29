3cdb2c780ea316b32ec4f9b0d58dd5876c4a72b9
Mathema_dx <necmettinkaradeli@gmail.com>
2023-04-29 23:33:25 +0300

    updated 29.04.2023 23:33

---
+++ src/test/java/video04_webElements/WebElements01.java
@@ -0,0 +1,55 @@
+package video04_webElements;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class WebElements01 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+/* Benim çözümüm :
+ // 1- https:www.amazon.com sayfasina gidin
+        driver.navigate().to("https:www.amazon.com");
+        //2- arama cubuguna “Nutella” yazdirin
+        WebElement element = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+        element.sendKeys("Nutella");
+        //3- Nutella yazdiktan sonra ENTER’a basarak arama islemini yapin
+        element.submit();
+        //4- Bulunan sonuc sayisini yazdirin.
+        String result = driver.findElement(By.xpath("//*[@id=\"search\"]/span/div/h1/div/div[1]/div/div/span[1]")).getText();
+        System.out.println("result = " + result);
+ */
+        // Ahmet hocanın çözümü
+
+        // 1- https:www.amazon.com sayfasina gidin
+        driver.get("https:www.amazon.com");
+        //2- arama cubuguna “Nutella” yazdirin
+        WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("Nutella");
+        //3- Nutella yazdiktan sonra ENTER’a basarak arama islemini yapin
+        aramaKutusu.submit();
+        //4- Bulunan sonuc sayisini yazdirin.
+        WebElement sonucYaziElementi=driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
+        System.out.println(sonucYaziElementi.getText());
+
+        driver.close();
+
+
+    }
+
+
+
+
+
+
+
+
+
+    }
